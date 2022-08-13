4433c0dea074c6eb0d324b78e0c1d50791a3ba18
duffuwu <jvitorsa@gmail.com>
2022-08-13 16:32:58 -0300

    Last update had accidentally disabled the password verification script. It is now fixed.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const password = document.querySelector('input[name="password"');
 const confirmPassword = document.querySelector('input[name="confirm-password"');
+const wrongPasswordMessage = document.querySelector('.password-error');
 const form = document.querySelector('form');
+
 
 form.addEventListener('submit', (e) => {
     if (password.value != confirmPassword.value) {

6e8fedfc34f112aa0c205efe6e28afcbf83cec94
duffuwu <jvitorsa@gmail.com>
2022-08-13 12:11:46 -0300

    Added password validation script which prevents submitting the form if passwords dont match.

---
+++ script.js
@@ -0,0 +1,10 @@
+const password = document.querySelector('input[name="password"');
+const confirmPassword = document.querySelector('input[name="confirm-password"');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    if (password.value != confirmPassword.value) {
+        wrongPasswordMessage.setAttribute('style','opacity: 1');
+        e.preventDefault();
+    }
+});
